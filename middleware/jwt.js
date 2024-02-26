85b17373e2dea304c6b4ab1241d4e391b6aed455
Aniket-Patidar <aniketpatidar60@gmail.com>
2024-02-26 16:56:40 +0530

    Update jwt.js

--- middleware/jwt.js
+++ middleware/jwt.js
@@ -2,8 +2,8 @@
 const jwt = require('jsonwebtoken');
 
 function isLoggedIn(req, res, next) {
-    // const authHeader = req.headers['authorization'];
-    const authHeader = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWRiNTRhNjI4MjFjZWVjZjFiOTZiM2MiLCJpYXQiOjE3MDg5NDQ1NzYsImV4cCI6MTcwOTAzMDk3Nn0.u7vk5OXNZ9YSo98Kg6l319idrKI3AZXazydO1bYnais";
+    const authHeader = req.headers['authorization'];
+    // const authHeader = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWRiNTRhNjI4MjFjZWVjZjFiOTZiM2MiLCJpYXQiOjE3MDg5NDQ1NzYsImV4cCI6MTcwOTAzMDk3Nn0.u7vk5OXNZ9YSo98Kg6l319idrKI3AZXazydO1bYnais";
     const token = authHeader;
 
     if (token == null) return res.sendStatus(401);
