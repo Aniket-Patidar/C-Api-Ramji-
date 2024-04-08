e7c2956d9f64284bf2a8c0f3ae9749aa3e2e9162
Aniket-Patidar <aniketpatidar60@gmail.com>
2024-04-08 17:20:24 +0530

    update env and remove firebase file

--- module/dbconnection.js
+++ module/dbconnection.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect("mongodb+srv://aniketpatidar76:aniketpatidar76@cluster0.gntonq2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+        const conn = await mongoose.connect(process.env.MONGODB_URI);
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (err) {
         console.error(`Error: ${err.message}`);
